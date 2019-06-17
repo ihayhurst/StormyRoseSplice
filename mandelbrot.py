c110b97823f4cb58388d8a10b346a5b774e61753
Ian Hayhurst <ihayhurst@syngenta.org>
2019-06-17 11:29:14 +0100

    modified:   mandelbrot.py

--- mandelbrot.py
+++ mandelbrot.py
@@ -9,12 +9,12 @@ from matplotlib import pyplot as plt
 from matplotlib import colors
 
 #allow choice of opencl device
-ctx = cl.create_some_context(interactive=True)
+#ctx = cl.create_some_context(interactive=True)
 
 #Preselect opencl device (or set an env var  PYOPENCL_CTX='0:0' before running)
-#platform = cl.get_platforms()[0]  # Select the first platform [0]
-#device = platform.get_devices()[0]  # Select the first device on this platform [0]
-#ctx = cl.Context([device])
+platform = cl.get_platforms()[0]  # Select the first platform [0]
+device = platform.get_devices()[0]  # Select the first device on this platform [0]
+ctx = cl.Context([device])
 
 @jit
 def mandelbrot(c,maxiter):
@@ -71,6 +71,7 @@ def progressIndication(x, screenSize):
     if x%32==0:
         prog = round(x/screenSize*100)
         print(str(prog) + "% done", end="\r")
+
 @jit
 def mandelbrot_set(xmin,xmax,ymin,ymax,width,height,maxiter):
     r1 = np.linspace(xmin, xmax, width)
@@ -79,7 +80,6 @@ def mandelbrot_set(xmin,xmax,ymin,ymax,width,height,maxiter):
     for i in range(width):
         for j in range(height):
             n3[i,j] = mandelbrot(r1[i] + 1j*r2[j],maxiter)
-        progressIndication(i,height)    
     return (r1,r2,n3)
 
 def mandelbrot_set1(xmin,xmax,ymin,ymax,width,height,maxiter):
@@ -107,7 +107,6 @@ def mandelbrot_set3(xmin,xmax,ymin,ymax,width,height,maxiter):
     c = np.ravel(c)
     n3 = mandelbrot_gpu(c,maxiter)
     n3 = n3.reshape((width,height))
-    return (r1,r2,n3.T)
     return (r1,r2,n3)
 
 
@@ -121,7 +120,7 @@ def mandelbrot_image(xmin,xmax,ymin,ymax,width,height,maxiter):
     #plt.xticks(ticks, x_ticks)
     #y_ticks = ymin + (ymax-ymin)*ticks/width
     #plt.yticks(ticks, y_ticks)
-    #ax.set_title(cmap)
+    ax.set_title(cmap)
     ax.imshow(z.T,cmap=cmap,origin='lower') 
     fig.savefig('plot.png')
     print('Created plot\n')
@@ -129,8 +128,8 @@ def mandelbrot_image(xmin,xmax,ymin,ymax,width,height,maxiter):
 
 def mandelbrot_image1(xmin,xmax,ymin,ymax,width,height,maxiter):
     print (xmin,xmax,ymin,ymax,width,height,maxiter)
-    x,y,z = mandelbrot_set3(xmin,xmax,ymin,ymax,width,height,maxiter)
-    image = Image.fromarray(z)
+    x,y,z = mandelbrot_set(xmin,xmax,ymin,ymax,width,height,maxiter)
+    image = Image.fromarray(z.T)
     image.save("plot.tiff")
     return
 
@@ -146,14 +145,10 @@ def main(args=None):
     #Offset to center the image
     #offset = -width*1.2/2-(height/2)*1j
     #Increase iterations to impove the quality of the image
-    maxiter = 128
-    # delta = mandelbrot_gpu(c, MAX_ITER) 
+    maxiter = 2048
     #make the (xmin,xmax,ymin,ymax,width,height,maxiter):
-    mandelbrot_image1(-2.0,0.5,-1.25,1.25,width,height,maxiter)
-    #Save and show the file
+    mandelbrot_image(-2.0,0.5,-1.25,1.25,width,height,maxiter)
     print("Image complete!")
-    #image = Image.fromarray(data)
-    #image.save("plot.tiff")
     sys.exit(1)
 
 if __name__ == '__main__':
