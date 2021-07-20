5d64767c6d8f0d671417667711a5d37f150d320d
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2021-07-20 21:54:00 +0100

    Added support for rendering regions of images separately, and then combining This can either have a huger performance improvement, memory reduction, or both

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # coding: utf-8
-import sys
+import sys, math, copy, os
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
 from numba import jit, int32, complex64
+from functools import cmp_to_key
 import pyopencl as cl
 from matplotlib import pyplot as plt
 from matplotlib import colors
@@ -58,14 +59,14 @@ def progressIndication(x, screenSize):
         print(str(prog) + "% done", end="\r")
 
 #Used for processing on CPU
-def mandel_iterate_bypixel(xmin, xmax, ymin, ymax, width, height, maxiter):
+def mandelbrot_iterate_bypixel(xmin, xmax, ymin, ymax, width, height, maxiter):
     r1 = np.arange(xmin, xmax, (xmax - xmin) / width)
     r2 = np.arange(ymin, ymax, (ymax - ymin) / height) 
     data = np.zeros((height, width, 3), dtype = np.uint8)
     for x in range(0, height - 1):
         for y in range(0, width - 1):
             c = r1[y] + r2[x] * 1j
-            delta = mandelbrot(c, maxiter) 
+            delta = mandelbrot(c, maxiter)
             red   = int(delta % 255)
             green = int(delta % 128)
             blue  = int(delta % 64)
@@ -75,7 +76,7 @@ def mandel_iterate_bypixel(xmin, xmax, ymin, ymax, width, height, maxiter):
     return(data)
 
 #Used for processing on GPU
-def mandel_iterate_byarray(xmin, xmax, ymin, ymax, width, height, maxiter):
+def mandelbrot_iterate_byarray(xmin, xmax, ymin, ymax, width, height, maxiter):
     r1 = np.arange(xmin, xmax, (xmax - xmin) / width)
     r2 = np.arange(ymin, ymax, (ymax - ymin) / height) * 1j
     c = r1 + r2[:,np.newaxis]
@@ -84,69 +85,177 @@ def mandel_iterate_byarray(xmin, xmax, ymin, ymax, width, height, maxiter):
     n3 = (n3.reshape((height, width)) / float(n3.max()) * 255.).astype(np.uint8)
     return (n3)
 
-def mandel_iterate(xmin, xmax, ymin, ymax, width, height, maxiter):
+def mandelbrot_iterate(xmin, xmax, ymin, ymax, width, height, maxiter):
     if settings.iterateMethod == 'array':
-        return mandel_iterate_byarray(xmin, xmax, ymin, ymax, width, height, maxiter)
+        return mandelbrot_iterate_byarray(xmin, xmax, ymin, ymax, width, height, maxiter)
     elif settings.iterateMethod == 'pixel':
-        return mandel_iterate_bypixel(xmin, xmax, ymin, ymax, width, height, maxiter)
+        return mandelbrot_iterate_bypixel(xmin, xmax, ymin, ymax, width, height, maxiter)
     else:
         print(f'Unrecognised iteration method "{settings.iterateMethod}", exiting')
         exit(1)
 
-def mandelbrot_image_mpl(xmin, xmax, ymin, ymax, width, height, maxiter):
-    #Displays with matplotlib
-    cmap = 'twilight'
-    my_dpi = 100
-    print(xmin, xmax, ymin, ymax, width, height, maxiter)
-    z = mandel_iterate(xmin, xmax, ymin, ymax, width, height, maxiter)
-    plot_title = cmap, xmin, xmax, ymin, ymax, width, height, maxiter
-
-    fig, ax = plt.subplots(figsize = (width / my_dpi, height / my_dpi), dpi = my_dpi)
-
-    ax.axis("off")
-    norm = colors.PowerNorm(0.5)
-    ax.imshow(z, cmap=cmap, norm=norm, origin='lower')
-    fig.savefig('plot.png',bbox_inches='tight', pad_inches = 0)
-    print('Created plot using matplotlib\n')
-    plt.clf()
-
-def mandelbrot_image_pil(xmin, xmax, ymin, ymax, width, height, maxiter):
-    #Displays with PIL (mono needs palette)
-    print(xmin, xmax, ymin, ymax, width, height, maxiter)
-    z = mandel_iterate(xmin, xmax, ymin, ymax, width, height, maxiter)
-
-    image = Image.fromarray(z)
-    #image.putpalette("P",[255, 0, 0, 254, 0, 0, 253, 0, 0, 244, 0, 0])
-    image.save("plot.png")
-    print('Created plot using PIL\n') 
-
-def main(args=None):
-    #Set required resolution, iteration depth and co-ordinate boundary below
-    resolutionMultiplier = 2
-    width = 2048 * resolutionMultiplier
-    height = 2048 * resolutionMultiplier
-    maxiter = 2048
-    xmin, xmax, ymin, ymax = -0.74877, -0.74872, 0.065053, 0.065103
-
-    if settings.imageMethod == 'mpl':
-        mandelbrot_image_mpl(xmin, xmax, ymin, ymax, width, height, maxiter)
-    elif settings.imageMethod == 'pil':
-        mandelbrot_image_pil(xmin, xmax ,ymin, ymax, width, height, maxiter)
-    else:
-        print(f'Unrecognised image method "{settings.imageMethod}", exiting')
+def mandelbrot_prepare(coords):
+    sideLength = coords['x']['max'] - coords['x']['min']
+    if sideLength != (coords['y']['max'] - coords['y']['min']):
+        print('X and Y length must be the same')
         exit(1)
 
-    print("Image complete!")
+    class tile:
+        def __init__(self, total):
+            self.totalTiles = total
+            self.tilesPerSide = int(math.sqrt(self.totalTiles))
+            self.length = int(imageSettings.length / self.tilesPerSide)
+            self.maxIter = imageSettings.maxIter
+
+        def setXInfo(self, xCount):
+            self.x['min'] = coords['x']['min'] + ((sideLength / self.tilesPerSide) * xCount)
+            self.x['max'] = coords['x']['min'] + ((sideLength / self.tilesPerSide) * (xCount + 1))
+
+        def setYInfo(self, yCount):
+            self.y['min'] = coords['y']['min'] + ((sideLength / self.tilesPerSide) * yCount)
+            self.y['max'] = coords['y']['min'] + ((sideLength / self.tilesPerSide) * (yCount + 1))
+
+        x = {}
+        y = {}
+
+    tileCount = imageSettings.tilesPerSide ** 2
+    tileInfo = tile(tileCount)
+    tiles = []
+
+    for x in range(0, tileInfo.tilesPerSide):
+        tempTile = tile(tileCount)
+        tempTile.setXInfo(x) #Work out x min and max values for that tile
+        for y in range(0, tileInfo.tilesPerSide):
+            tempTile.id = (tileInfo.tilesPerSide - y) + (x * tileInfo.tilesPerSide) #Get the tile ID, using x and y
+            tempTile.setYInfo(y) #Work out y min and max values for that tile
+
+            tiles.append(copy.deepcopy(tempTile))
+            tiles[len(tiles) - 1].x = copy.deepcopy(tempTile.x)
+            tiles[len(tiles) - 1].y = copy.deepcopy(tempTile.y)
+
+    def orderById(a, b):
+        if a.id > b.id:
+            return 1
+        else:
+            return -1
+    tiles.sort(key = cmp_to_key(orderById))
+
+    for tile in tiles:
+        print(f'{tile.id}: {tile.x["min"]}, {tile.x["max"]}, {tile.y["min"]}, {tile.y["max"]}, {tile.length}, {tile.length}, {tile.maxIter}')
+        data = mandelbrot_iterate(tile.x['min'], tile.x['max'], tile.y['min'], tile.y['max'], tile.length, tile.length, tile.maxIter)
+        mandelbrot_image_mpl(data, tile, f'plots/plot{tile.id}.png')
+
+    #Merge all the images into 1
+    combine_images('output')
+
+def mandelbrot_image_mpl(data, tile, outputPath):
+    cmap = 'twilight'
+    imageDpi = 100
+
+    fig, ax = plt.subplots(figsize = ((tile.length / imageDpi), (tile.length / imageDpi)), dpi = imageDpi)
+
+    ax.axis('off')
+    norm = colors.PowerNorm(0.5 * imageSettings.colourMultiplier)
+    ax.imshow(data, cmap=cmap, norm=norm, origin='lower')
+
+    if not os.path.isdir('plots'):
+        os.mkdir('plots')
+
+    fig.tight_layout()
+    fig.savefig(outputPath, bbox_inches='tight', pad_inches=0)
+    print(f'Rendered region {tile.id}/{tile.totalTiles}\n')
+
+    plt.clf()
+    plt.close()
+
+def combine_images(outputImage):
+    outputPath = f'{outputImage}.{imageSettings.fileType}'
+    print(f'Exporting images to {outputPath}')
+
+    images = []
+    for file in os.listdir('plots'):
+        if file.endswith(imageSettings.fileType):
+            images.append(f'plots/{file}')
+
+    def plotSort(a, b):
+        #Remove 'plots/plot' and file extension from files, then compare numerically
+        a = a.split('.')[0].replace('plots/plot', '')
+        b = b.split('.')[0].replace('plots/plot', '')
+        if int(a) > int(b):
+            return 1
+        else:
+            return -1
+
+    images.sort(key = cmp_to_key(plotSort))
+
+    shape = (imageSettings.tilesPerSide, imageSettings.tilesPerSide)
+    length = int(imageSettings.length / imageSettings.tilesPerSide)
+    size  = (length, length)
+
+    shape = shape if shape else (1, len(images))
+    image_size = (length * shape[1], length * shape[0])
+    image = Image.new('RGB', image_size)
+    
+    #Paste images into final image
+    for row in range(shape[0]):
+        for col in range(shape[1]):
+            offset = length * col, length * row
+
+            imageCount = col * shape[1] + row
+            images[imageCount] = Image.open(images[imageCount])
+
+            image.paste(images[imageCount], offset)
+            images[imageCount] = None
+    image.save(outputPath, imageSettings.fileType)
+
+
+def main():
+    #Set co-ordinate boundary below
+    coords = {
+        'default': {
+            'x': {
+                'min': -0.74877,
+                'max': -0.74872
+            },
+            'y': {
+                'min': 0.065053,
+                'max': 0.065103
+            }
+        }
+    }
+
+    #Cleanup old plots
+    oldPlots = os.listdir('plots')
+    if oldPlots != []:
+        for file in oldPlots:
+          os.remove(f'plots/{file}')
+
+    mandelbrot_prepare(coords['default'])
+
+    print('Image complete!')
     sys.exit(1)
 
+#Program operation settings
 class settings:
-    iterateMethod = 'pixel'
-    imageMethod = 'mpl'
-    context = ''
+    iterateMethod = 'pixel' #Handled by arguments, 'pixel' uses cpu
+    context = '' #Handled by arguments
+    coreCount = 1
+
+#Settings to generate image from
+class imageSettings:
+    def __init__(self):
+        self.fileType = 'png'
+        self.tilesPerSide = 2
+        self.resolutionMultiplier = 1
+        self.length = 4096 * self.resolutionMultiplier
+        self.maxIter = 2048
+        self.colourMultiplier = 1
+imageSettings = imageSettings()
 
 class deviceSettings:
     defaultPlatform = 0
     defaultDevice = 0
+    #Handled by arguments from here
     platform = None
     device = None
     useSettings = False
@@ -211,9 +320,4 @@ if __name__ == '__main__':
                 deviceSettings.device = deviceSettings.defaultDevice
 
         settings.context = createContext()
-
-    try:
         main()
-    except ValueError:
-        print("Give me something to do")
-        sys.exit(1)
